b25d467b951d255044953216feb0fd0abb2f3e43
Drew Stonebraker <drew.stonebraker@gmail.com>
2016-12-19 16:48:50 +0000

    add sound/music controls

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@
 
 $(function() {
     
+    var soundEffectsOn = true;
+    
+    //load background map image
     new Promise(function(resolve) {
         var viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
     
@@ -22,16 +25,16 @@ $(function() {
         $("#view").prepend(background);
         $("#background").attr('alt', "A map of the Pokemon world in the background")
         
-    })
+    }) // then set horizontal scrolling animation
         .then(function() {
         
     	var backgroundScrollSpeed = 150;//higher number is slower
-    	var backgroundTiming;
-    	var backgroundTransition;
-    	var backgroundMargin;
-    	var animateBackground;
+    	var backgroundTiming; //time in ms for full scroll of one direction
+    	var backgroundTransition; //css transition value used for animation
+    	var backgroundMargin; //number of horizontal pixels to scroll each direction
+    	var animateBackground; //a setInterval function
     	
-    	function updateBackgroundTiming() {		
+    	function updateBackgroundTiming() {	//triggered on load and resize.  also starts/restarts animation
     		backgroundMargin = Math.floor($("#background").width() - document.documentElement.clientWidth);
     		backgroundTiming = backgroundMargin * backgroundScrollSpeed;
     		
@@ -79,5 +82,27 @@ $(function() {
     }); // end "then" attached to background image load promise
     
     
+    //set music toggle functionality
+    $("#controls__toggle--music").click(function() {
+        var music =document.getElementById('music');
+      if (music.paused == false) {
+          music.pause();
+          $(this).css("background-position-y", "32px").attr("title", "Music On");
+      } else {
+          music.play();
+          $(this).css("background-position-y", "").attr("title", "Music Off");
+      }
+    });
+    
+    //set sound effects toggle functionality
+    $("#controls__toggle--sound").click(function() {
+      if (soundEffectsOn) {
+          soundEffectsOn = false;
+          $(this).css("background-position-y", "32px").attr("title", "Sound Effects On");
+      } else {
+          soundEffectsOn = true;
+          $(this).css("background-position-y", "").attr("title", "Sound Effects Off");
+      }
+    });
 	
 })
